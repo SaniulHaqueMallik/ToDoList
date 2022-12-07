fa13f40d9e6d2ea209a00863360f85fbc70a5706
Saniul <saniulmallik@techis.io>
2022-12-07 11:44:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import {Task} from './Task'
 
 function App() {
-  return (
+  const [todoList,setTodoList] = useState([]);
+  const [newTask, setNewTask]= useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)}; //we are using event to grab the value of the input
+
+  const addTask = () => {
+    // // the next list will make an arrey for todolist and add newtask to todolist
+    // const newTodoList = [...todoList, newTask];
+    // setTodoList(newTodoList);
+
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length -1].id +1 ,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+  // like ---if 
+
+  // const deleteTask = (taskName) => {
+  //   const newTodoList = todoList.filter((task) => {
+      // if(task === taskName){
+      //   return false;
+      // }{
+      //   return true;
+      // }
+      const deleteTask = (id) => {
+        const newTodoList = todoList.filter((task) => {
+      return task.id !== id;
+    });
+    setTodoList(newTodoList); 
+
+  };
+  
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if(task.id === id) {
+          return { ...task, completed: true};
+        }else{
+          return task;
+        }
+      })
+    )
+
+  }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='addTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}> Add Task</button>
+      </div>
+      <div className='list'>
+       {todoList.map((task) => {
+           return <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask} completed={task.completed} completeTask={completeTask}/>;
+       })}
+      </div>
+      
     </div>
   );
 }
